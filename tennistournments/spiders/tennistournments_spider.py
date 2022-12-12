e78a06e6cc33ba436b1f6fe35b9e305e6705f6c1
sunkari vasu <sunkarivasu1@gmail.com>
2022-12-12 16:51:40 +0530

    fetched cookies from the browser

--- tennistournments/spiders/tennistournments_spider.py
+++ tennistournments/spiders/tennistournments_spider.py
@@ -15,7 +15,6 @@ class TennisTournments_spider(scrapy.Spider):
         'accept': '*/*',
         'accept-language': 'en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7,te;q=0.6',
         'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-        # 'cookie': 'st=l=3081&exp=45269.7684725231; _ga=GA1.3.1505240466.1670606802; _gid=GA1.3.1901282358.1670606802; _hjSessionUser_906305=eyJpZCI6IjU1ZjkwMjJlLTY4OWMtNWRmOS1hMmRjLTA5ZWExZTFkNzExZSIsImNyZWF0ZWQiOjE2NzA2MDY4MDUzNzEsImV4aXN0aW5nIjp0cnVlfQ==; ASP.NET_SessionId=vhkrsb1en1yzidfpsnqwqcsg; _hjSession_906305=eyJpZCI6ImZmODZjYzZhLTM1N2EtNGVhNC1hYWI3LTE4YzNiZjNmMjlhZCIsImNyZWF0ZWQiOjE2NzA2Nzk1OTE5NzksImluU2FtcGxlIjpmYWxzZX0=; _hjAbsoluteSessionInProgress=0; _hjIncludedInSessionSample=0; _gat=1',
         'origin': 'https://tournaments.tennis.com.au',
         'referer': 'https://tournaments.tennis.com.au/find?DateFilterType=0&StartDate=2022-12-05&EndDate=2023-03-09&Distance=15&page=1',
         'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
@@ -27,18 +26,7 @@ class TennisTournments_spider(scrapy.Spider):
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
         'x-requested-with': 'XMLHttpRequest',
     }
-
-    cookies = {
-        'st': 'l=3081&exp=45269.7684725231',
-        '_ga': 'GA1.3.1505240466.1670606802',
-        '_gid': 'GA1.3.1901282358.1670606802',
-        '_hjSessionUser_906305': 'eyJpZCI6IjU1ZjkwMjJlLTY4OWMtNWRmOS1hMmRjLTA5ZWExZTFkNzExZSIsImNyZWF0ZWQiOjE2NzA2MDY4MDUzNzEsImV4aXN0aW5nIjp0cnVlfQ==',
-        'ASP.NET_SessionId': 'vhkrsb1en1yzidfpsnqwqcsg',
-        '_hjSession_906305': 'eyJpZCI6ImZmODZjYzZhLTM1N2EtNGVhNC1hYWI3LTE4YzNiZjNmMjlhZCIsImNyZWF0ZWQiOjE2NzA2Nzk1OTE5NzksImluU2FtcGxlIjpmYWxzZX0=',
-        '_hjAbsoluteSessionInProgress': '0',
-        '_hjIncludedInSessionSample': '0',
-        '_gat': '1',
-    }
+    cookies = {}
 
     body = "Page=" + str(pageNo) + "&TournamentExtendedFilter.SportID=0&TournamentFilter.Q=&TournamentFilter.DateFilterType=0&TournamentFilter.StartDate=2022-12-05&TournamentFilter.EndDate=2023-03-09&TournamentFilter.PostalCode=&TournamentFilter.Distance=15&TournamentExtendedFilter.OrganizationStateList%5B0%5D=false&TournamentExtendedFilter.OrganizationStateList%5B1%5D=false&TournamentExtendedFilter.OrganizationStateList%5B2%5D=false&TournamentExtendedFilter.OrganizationStateList%5B3%5D=false&TournamentExtendedFilter.OrganizationStateList%5B4%5D=false&TournamentExtendedFilter.OrganizationStateList%5B5%5D=false&TournamentExtendedFilter.OrganizationStateList%5B6%5D=false&TournamentExtendedFilter.OrganizationStateList%5B7%5D=false&TournamentExtendedFilter.TournamentCategoryIDList%5B0%5D=false&TournamentExtendedFilter.TournamentCategoryIDList%5B1%5D=false&TournamentExtendedFilter.TournamentCategoryIDList%5B2%5D=false&TournamentExtendedFilter.TournamentCategoryIDList%5B3%5D=false&TournamentExtendedFilter.TournamentCategoryIDList%5B4%5D=false&TournamentExtendedFilter.TournamentCategoryIDList%5B5%5D=false&TournamentExtendedFilter.TournamentCategoryIDList%5B6%5D=false&TournamentExtendedFilter.TournamentCategoryIDList%5B7%5D=false&TournamentExtendedFilter.TournamentCategoryIDList%5B8%5D=false&TournamentExtendedFilter.TournamentCategoryIDList%5B9%5D=false&TournamentExtendedFilter.TournamentCategoryIDList%5B10%5D=false&TournamentExtendedFilter.TournamentCategoryIDList%5B11%5D=false&TournamentExtendedFilter.TournamentCategoryIDList%5B12%5D=false&TournamentExtendedFilter.OrganizationCourtSurfaceTypeList%5B0%5D=false&TournamentExtendedFilter.OrganizationCourtSurfaceTypeList%5B1%5D=false&TournamentExtendedFilter.OrganizationCourtSurfaceTypeList%5B2%5D=false&TournamentExtendedFilter.OrganizationCourtSurfaceTypeList%5B3%5D=false&TournamentExtendedFilter.OrganizationCourtSurfaceTypeList%5B4%5D=false&TournamentExtendedFilter.EventGameTypeIDList%5B0%5D=false&TournamentExtendedFilter.EventGameTypeIDList%5B1%5D=false&TournamentExtendedFilter.EventGameTypeIDList%5B2%5D=false&TournamentExtendedFilter.EventGameTypeIDList%5B3%5D=false&TournamentExtendedFilter.EventGameTypeIDList%5B4%5D=false&X-Requested-With=XMLHttpRequest"
 
@@ -56,6 +44,14 @@ class TennisTournments_spider(scrapy.Spider):
         self.body = "Page=" + str(self.pageNo) + "&TournamentExtendedFilter.SportID=0&TournamentFilter.Q=&TournamentFilter.DateFilterType=0&TournamentFilter.StartDate=2022-12-05&TournamentFilter.EndDate=2023-03-09&TournamentFilter.PostalCode=&TournamentFilter.Distance=15&TournamentExtendedFilter.OrganizationStateList%5B0%5D=false&TournamentExtendedFilter.OrganizationStateList%5B1%5D=false&TournamentExtendedFilter.OrganizationStateList%5B2%5D=false&TournamentExtendedFilter.OrganizationStateList%5B3%5D=false&TournamentExtendedFilter.OrganizationStateList%5B4%5D=false&TournamentExtendedFilter.OrganizationStateList%5B5%5D=false&TournamentExtendedFilter.OrganizationStateList%5B6%5D=false&TournamentExtendedFilter.OrganizationStateList%5B7%5D=false&TournamentExtendedFilter.TournamentCategoryIDList%5B0%5D=false&TournamentExtendedFilter.TournamentCategoryIDList%5B1%5D=false&TournamentExtendedFilter.TournamentCategoryIDList%5B2%5D=false&TournamentExtendedFilter.TournamentCategoryIDList%5B3%5D=false&TournamentExtendedFilter.TournamentCategoryIDList%5B4%5D=false&TournamentExtendedFilter.TournamentCategoryIDList%5B5%5D=false&TournamentExtendedFilter.TournamentCategoryIDList%5B6%5D=false&TournamentExtendedFilter.TournamentCategoryIDList%5B7%5D=false&TournamentExtendedFilter.TournamentCategoryIDList%5B8%5D=false&TournamentExtendedFilter.TournamentCategoryIDList%5B9%5D=false&TournamentExtendedFilter.TournamentCategoryIDList%5B10%5D=false&TournamentExtendedFilter.TournamentCategoryIDList%5B11%5D=false&TournamentExtendedFilter.TournamentCategoryIDList%5B12%5D=false&TournamentExtendedFilter.OrganizationCourtSurfaceTypeList%5B0%5D=false&TournamentExtendedFilter.OrganizationCourtSurfaceTypeList%5B1%5D=false&TournamentExtendedFilter.OrganizationCourtSurfaceTypeList%5B2%5D=false&TournamentExtendedFilter.OrganizationCourtSurfaceTypeList%5B3%5D=false&TournamentExtendedFilter.OrganizationCourtSurfaceTypeList%5B4%5D=false&TournamentExtendedFilter.EventGameTypeIDList%5B0%5D=false&TournamentExtendedFilter.EventGameTypeIDList%5B1%5D=false&TournamentExtendedFilter.EventGameTypeIDList%5B2%5D=false&TournamentExtendedFilter.EventGameTypeIDList%5B3%5D=false&TournamentExtendedFilter.EventGameTypeIDList%5B4%5D=false&X-Requested-With=XMLHttpRequest"
 
     def parse(self,response):
+
+        recievedCookies = response.headers.getlist('Set-Cookie')[0].decode("utf-8").split(";")[0].split("=")
+        print("cookie",recievedCookies)
+
+        for i in range(len(recievedCookies)//2):
+            self.cookies[recievedCookies[(2*i)]] = recievedCookies[(2*i)+1]
+
+        print(self.cookies)
         yield scrapy.Request(self.url,method="POST",body=self.body,headers=self.headers,callback = self.checkForMoreResults,cookies=self.cookies)
 
     def checkForMoreResults(self,response):
@@ -66,10 +62,9 @@ class TennisTournments_spider(scrapy.Spider):
             self.pageNo += 1
             self.changeBodyBasedOnPageNo()
             yield scrapy.Request(self.url, method="POST", body=self.body, headers=self.headers,
-                                 callback=self.checkForMoreResults, cookies=self.cookies)
+                                 callback=self.checkForMoreResults,cookies=self.cookies)
         else:
-            yield scrapy.Request(self.url, method="POST", body=self.body, callback=self.start_parsing, headers=self.headers,
-                                 cookies=self.cookies,dont_filter=True)
+            yield scrapy.Request(self.url, method="POST", body=self.body, callback=self.start_parsing, headers=self.headers,cookies=self.cookies,dont_filter=True)
 
     def start_parsing(self,response):
 
